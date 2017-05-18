c3fc2375c8d8596aa095e60f9bc3c71a3e25b1fc
EvilAccountant <344890472@qq.com>
2017-05-18 14:22:41 +0800

    更个新

--- src/test/java/com/imooc/service/GirlServiceTest.java
+++ src/test/java/com/imooc/service/GirlServiceTest.java
@@ -1,15 +1,27 @@
 package com.imooc.service;
 
+import com.imooc.GirlApplicationTests;
+import com.imooc.domain.Girl;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.Assert.*;
+//@RunWith(SpringRunner.class)
+////表示在测试环境下运行
+//@SpringBootTest
+////表示运行整个SpringBoot
 
-/**
- * Created by Dell on 2017-5-18.
- */
-public class GirlServiceTest {
+//继承ApplicationTests则不需要以上两个注解
+public class GirlServiceTest extends GirlApplicationTests {
+
+    @Autowired
+    private GirlService girlService;
+
     @Test
-    public void findOne() throws Exception {
+    public void findOneTest(){
+        Girl girl=girlService.findOne(10);//获取对象
+        Assert.assertEquals(new Integer(28),girl.getAge());//将对象的age与输入值进行比对
+
     }
 
 }
